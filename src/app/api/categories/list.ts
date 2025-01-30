2e4eea7d5d3da2c54d776692f968aeca0749749c
Bruno Alfieri <brunoferreiraalfieri@gmail.cokm>
2025-01-30 09:16:29 -0400

    feat: create api categories

---
+++ src/app/api/categories/list.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '../prisma';
+
+export async function categoriesList() {
+  try {
+    const categories = await prisma.productsCategory.findMany({
+      omit: {
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        result: categories,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    return NextResponse.json(
+      { message: 'Error fetching categories' },
+      { status: 500 }
+    );
+  }
+}
