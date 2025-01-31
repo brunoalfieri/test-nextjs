24fd953a60fd4578e5e6eaec39b7bbc8fa3b8cae
Bruno Alfieri <brunoferreiraalfieri@gmail.cokm>
2025-01-31 00:20:36 -0400

    feat: create form, sidebar and composition pages

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,51 +1,18 @@
-'use client';
-import { Card } from '@/components/card';
-import { CardCategory } from '@/components/card/Category';
-import { CardDescription } from '@/components/card/Description';
-import { CardPrice } from '@/components/card/Price';
-import { CardTitle } from '@/components/card/Title';
-import { EProductAction, productsService } from '@/service/actions/products';
-import { useQuery } from '@tanstack/react-query';
+import { productsService } from '@/service/actions/products';
 import { HeaderProductsTable } from './Header';
+import { List } from './List';
+import { Sidebar } from './Sidebar';
 
-export default function Products() {
-  const { isLoading, data } = useQuery({
-    queryKey: [EProductAction.PRODUCTS_TABLE],
-    queryFn: (context) =>
-      productsService.table({
-        context,
-        params: {
-          page: 1,
-          pageSize: 10,
-        },
-      }),
-  });
-
-  if (isLoading) return <div>Loading</div>;
-
+export default async function Products() {
+  const { result } = await productsService.priceRange({});
+  console.log('RESULT', result);
   return (
-    <>
+    <div className="grid grid-cols-[max-content,1fr] grid-rows-[max-content,1fr] h-full">
+      <Sidebar {...result} />
       <HeaderProductsTable />
-      <main className="w-full h-full p-10">
-        <ul className="grid grid-cols-3 lg:grid-cols-4 gap-10">
-          {data?.result.map((product) => {
-            return (
-              <Card.Root
-                key={product.id}
-                as="li"
-                className="max-h-[500px]"
-                href={`/products/${product.id}`}
-              >
-                <Card.Image src={product.image} alt="Imagem ilustrativa" />
-                <CardTitle>{product.name}</CardTitle>
-                <CardDescription>{product.description}</CardDescription>
-                <CardPrice value={product.price} oldValue="200000" />
-                <CardCategory>{product.category.name}</CardCategory>
-              </Card.Root>
-            );
-          })}
-        </ul>
+      <main className="w-full h-full p-10 overflow-y-auto">
+        <List />
       </main>
-    </>
+    </div>
   );
 }
---
+++ src/components/Forms/ProductsFilter/SearchField.tsx
@@ -0,0 +1,34 @@
+import { Input } from '@/components/inputs/Input';
+import { Typography } from '@/components/typography/Typography';
+import { FilterSchema } from '@/service/actions/products/schema';
+import { SearchRounded } from '@mui/icons-material';
+import { useFormContext } from 'react-hook-form';
+
+export function SearchField() {
+  const formContext = useFormContext<FilterSchema>();
+  const searchRegister = formContext.register('search');
+  return (
+    <div>
+      <Typography.Label as="label" htmlFor="search">
+        Search
+      </Typography.Label>
+      <Input.Root formNoValidate>
+        <Input.Text.Root
+          {...searchRegister}
+          size="sm"
+          type="search"
+          placeholder="Search a product with text"
+        >
+          <Input.Text.IconRight
+            type="button"
+            buttonProps={{
+              type: 'submit',
+              className: 'hover:scale-125 transition-all',
+            }}
+            icon={SearchRounded}
+          />
+        </Input.Text.Root>
+      </Input.Root>
+    </div>
+  );
+}
