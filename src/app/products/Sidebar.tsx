0a6a6f5182447908e60fce7f0ef205f74a73595b
Bruno Alfieri <brunoferreiraalfieri@gmail.cokm>
2025-01-31 00:20:36 -0400

    feat: create form, sidebar and composition pages

---
+++ src/app/products/Sidebar.tsx
@@ -0,0 +1,9 @@
+import { FormProductsFilter } from '@/components/Forms/ProductsFilter';
+
+export function Sidebar(props: { cheaper: number; mostExpensive: number }) {
+  return (
+    <aside className="col-start-1 col-end-2 row-start-1 row-end-3 shadow-md w-80 p-4 h-full flex flex-col">
+      <FormProductsFilter {...props} />
+    </aside>
+  );
+}
